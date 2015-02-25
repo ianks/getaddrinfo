65d6aad639e148df99f981b7a04367dc7ee419c2
Ian Ker-Seymer <i.kerseymer@gmail.com>
2015-02-24 23:25:20 -0700

    Make bench prettier

--- bench/bench.rb
+++ bench/bench.rb
@@ -3,11 +3,11 @@ require 'gethostbyname'
 require 'socket'
 
 Benchmark.ips do |x|
-  x.report 'gethostbyname' do
+  x.report 'Gethostbyname.gethostbyname =>' do
     Gethostbyname.gethostbyname 'apple.com'
   end
 
-  x.report 'gethostbyname' do
+  x.report 'Socket.gethostbyname =>' do
     Socket.gethostbyname 'apple.com'
   end
 end
